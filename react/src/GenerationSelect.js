2c00db41652873b58cb1e563c51e9c9d4fe83a5e
Carter Davis <cartmandavis@gmail.com>
2026-09-13 21:51:55 -0600

    Setup basic React project

---
+++ react/src/GenerationSelect.js
@@ -0,0 +1,14 @@
+export function GenerationSelect() {
+  return (<select title="Select the generation.">
+    <option value="1">RBY</option>
+    <option value="2">GSC</option>
+    <option value="3">ADV</option>
+    <option value="4">DPP</option>
+    <option value="5">B/W</option>
+    <option value="6">ORAS</option>
+    <option value="7">USUM</option>
+    <option value="8">SWSH</option>
+    <option value="9">S/V</option>
+    <option value="10">CHAMP</option>
+  </select>)
+}
